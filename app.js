b82298fc7a3521add78aeaf7b6659888e0ad167d
mayeshm1 <mayeshcule@gmail.com>
2023-08-04 17:23:40 +0530

    new commit

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-let demo = React.createElement(
-	"h1", { style: { color: "green" } }, "Hello World"
-)
-const main = ReactDOM.createRoot(
-	document.getElementById('root')
+// let demo = React.createElement(
+// 	"h1", { style: { color: "green" } }, "Hello World"
+// )
+
+const Cricket = () =>{
+    return <div>Rohit</div>
+
+}
+const cricket2 = 
+         <React.Fragment>      
+              <div>Hello sachin</div>
+			  <h1>Hello Dhoni</h1> 
+			  </React.Fragment>
+const Cricket1 = 
+<div>
+	{cricket2}
+	{Cricket()}
+	<Cricket></Cricket>
+	<Cricket/>
+	<h1>Kohli</h1>
+</div>
+
+const main = ReactDOM.createRoot(document.getElementById('root')
 );
 
-main.render(demo)
+main.render(Cricket1);
